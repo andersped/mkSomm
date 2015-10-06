ad3f81fba67d86072699d499d292404156ec817b
andersped <janderspederson@gmail.com>
2015-10-06 09:11:27 -0700

    Fixed directive bug

--- mkSommApp/app/assets/javascripts/directives.js
+++
@@ -1,9 +0,0 @@
-mkSomm.directive('backImg', function(){
-    return function(scope, element, attrs){
-        var url = attrs.backImg;
-        element.css({
-            'background-image': 'url(' + url +')',
-            'background-size' : 'cover'
-        });
-    };
-});
